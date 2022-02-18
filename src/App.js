d1372ffe51e9b595d98ded1db54b28a3a4096b79
Wanthanai Kaiphet <wanthanai4work@gmail.com>
2022-02-18 23:47:08 +0700

    it's done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+//! i18next
+import { useTranslation } from 'react-i18next'
+import i18n from "i18next";
+
+
+//! App Commponent
 function App() {
+  //! useTranslation
+  const { t } = useTranslation();
+
+  //! Functions
+  // change language
+  const changeLanguage = (lang) => {
+    i18n.changeLanguage(lang);
+  }
+
+  //! Return Component
   return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+    <center>
+      <h1>{t('welcome_to_react')}</h1>
+
+      <select onChange={(e) => changeLanguage(e.target.value)} style={{ padding: '10px', fontSize: '2rem' }}>
+        <option value="th">th</option>
+        <option value="en">en</option>
+      </select>
+    </center>
+  )
 }
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//! Styles
 import './index.css';
+//! Components
 import App from './App';
+//! i18next
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+//! Languages
+import lEn from './assets/locales/en/translation.json'
+import lTh from './assets/locales/th/translation.json'
+
+
+i18n
+  .use(initReactI18next) // passes i18n down to react-i18next
+  .init({
+    // the translations
+    // (tip move them in a JSON file and import them,
+    // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+    resources: {
+      en: {
+        translation: lEn
+      },
+      th: {
+        translation: lTh
+      }
+    },
+    lng: "en", // if you're using a language detector, do not define the lng option
+    fallbackLng: "en",
+  });
 
 ReactDOM.render(
   <React.StrictMode>
